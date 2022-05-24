480dbdeaf43e528c51bc030d21d170a75ada08fd
Igor Renan <igorenansc@gmail.com>
2022-05-24 09:15:14 -0300

    initial commit

---
+++ app/controllers/persons_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class PersonsController < ApplicationController
+  def index; end
+end
---
+++ config/importmap.rb
@@ -0,0 +1,10 @@
+# Pin npm packages by running ./bin/importmap
+
+pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin 'jquery', to: 'https://code.jquery.com/jquery-3.6.0.min.js', preload: true
+pin 'semantic-ui', to: 'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js'
+
+pin_all_from 'app/javascript/controllers', under: 'controllers'
